beca8235dcda5b8cbd1dcc9e167562ef4c60eff2
Hendrik Pienaar <pienaahj@gmail.com>
2022-10-11 18:26:44 +0200

    removed async on routing components

--- src/router.js
+++ src/router.js
@@ -1,20 +1,14 @@
-import { defineAsyncComponent } from 'vue';
+
 import { createRouter, createWebHistory } from 'vue-router';
 
 import NotFound from './pages/NotFound.vue';
 import HomePage from './pages/HomePage.vue';
-// import AboutMe from './pages/AboutMe.vue';
-// import CoursePage from './pages/CoursePage.vue';
-// import EmploymentPage from './pages/EmploymentPage.vue';
-// import PersonalPage from './pages/PersonalPage.vue';
-// import SiteInfo from './pages/SiteInfo.vue';
+import AboutMe from './pages/AboutMe.vue';
+import CoursePage from './pages/CoursePage.vue';
+import EmploymentPage from './pages/EmploymentPage.vue';
+import PersonalPage from './pages/PersonalPage.vue';
+import SiteInfo from './pages/SiteInfo.vue';
 
-// define async components and only load when needed
-const CoursePage = defineAsyncComponent(() => import('./pages/CoursePage.vue'));
-const EmploymentPage = defineAsyncComponent(() => import('./pages/EmploymentPage.vue')); 
-const AboutMe = defineAsyncComponent(() => import('./pages/AboutMe.vue'));
-const PersonalPage = defineAsyncComponent(() => import('./pages/PersonalPage.vue'));    
-const SiteInfo = defineAsyncComponent(() => import('./pages/SiteInfo.vue'));
 
 const router = createRouter({
     history: createWebHistory(), // use the built in browser history
